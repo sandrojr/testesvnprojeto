addfe98e65c93cc9c945aa1273261ef8baacccea
ricairon@gmail.com <ricairon@gmail.com@ac61d1de-47b6-7190-42f4-3dcdb97d94fe>
2013-09-11 13:48:58 +0000

    Modificação 3

--- Projeto_Teste/src/padrao/Janela.java
+++ Projeto_Teste/src/padrao/Janela.java
@@ -5,6 +5,7 @@ public class Janela {
 	public static void main(String[] args) {
 		System.out.println("Syso demorado");
 		System.out.println("Estou fazendo uma modificação: Ricardo");
+		System.out.println("Modificação número 3: Ricardo Lopes");
 	}
 
 }
